5a616dc3737ce5f51e95553c77acf84c1f615851
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-12 00:09:42 +0545

    Modified App.jsx to match the paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import LoginRegister from "./pages/LoginRegister.jsx";
-import Homepage from "./pages/HomePage.jsx";
-import CartPage from "./pages/CartPage.jsx";
-import FavoritesPage from "./pages/Favorite.jsx";
-import CategoryPage from "./pages/CategoryPage.jsx";
-import ForgotPassword from "./pages/ForgotPassword.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Admin from "./pages/Admin.jsx";
-import Dashboard from "./pages/Dashboard.jsx"; // Import Dashboard
-import Users from "./pages/Users.jsx";
-import Orders from "./pages/Order.jsx";
-import ManageBooks from "./pages/ManageBooks.jsx";
-import Requests from "./pages/Requests.jsx";
-import Settings from "./pages/Settings.jsx";
-import ContactUs from "./pages/ContactUs.jsx";
-import DetailPage from "./pages/DetailPage.jsx";
 import BookForm from "./pages/BookForm.jsx";
-import BookForm2 from "./pages/BookForm2.jsx";
+import BookForm2 from "./pages/admin/BookForm2.jsx";
+import ManageBooks from "./pages/admin/ManageBooks.jsx";
+import Orders from "./pages/admin/Order.jsx";
+import Requests from "./pages/admin/Requests.jsx";
+import Settings from "./pages/admin/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import CategoryPage from "./pages/CategoryPage.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import DetailPage from "./pages/DetailPage.jsx";
+import FavoritesPage from "./pages/Favorite.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Homepage from "./pages/HomePage.jsx";
+import LoginRegister from "./pages/LoginRegister.jsx";
 
 const router = createBrowserRouter([
   {
